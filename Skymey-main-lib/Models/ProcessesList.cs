ab3d57be058a643ed4d258b05f693df92ec0d272
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-06 14:47:09 +0300

    add process model

---
+++ Skymey-main-lib/Models/ProcessesList.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_main_lib.Models
+{
+    public class ProcessesList
+    {
+        public string? Name { get; set; }
+        public string? Directory { get; set; }
+        public string? FileName { get; set; }
+        public string? Agruments { get; set; }
+    }
+}
---
+++ Skymey-main-lib/Models/ProcessesListShow.cs
@@ -0,0 +1,11 @@
+﻿namespace Skymey_main_lib.Models
+{
+    public class ProcessesListShow
+    {
+        public string? Name { get; set; }
+        public string? Directory { get; set; }
+        public string? FileName { get; set; }
+        public string? Agruments { get; set; }
+        public bool? Show { get; set; }
+    }
+}
