8fd7185ee6cf45f454db702c5dcfa0122277f795
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-06 17:34:42 +0300

    delete unused class

--- Skymey-main-lib/Models/JWT/TokenApiModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Skymey_main_lib.Models.JWT
-{
-    public class TokenApiModel
-    {
-        public string? AccessToken { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
