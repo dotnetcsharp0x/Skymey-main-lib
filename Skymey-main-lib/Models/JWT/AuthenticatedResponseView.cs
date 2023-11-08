2b763599923606076e1de8b530d55e1bad1ce07a
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-08 17:05:22 +0300

    add action on tokens

---
+++ Skymey-main-lib/Models/JWT/AuthenticatedResponseView.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Skymey_main_lib.Models.JWT
+{
+    public class AuthenticatedResponseView
+    {
+        [JsonProperty("accessToken")]
+        [JsonPropertyName("accessToken")]
+        public string? AccessToken { get; set; }
+        [JsonProperty("refreshToken")]
+        [JsonPropertyName("refreshToken")]
+        public string? RefreshToken { get; set; }
+    }
+}
