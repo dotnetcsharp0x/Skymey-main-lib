6f952863588d9fb9d514f6e0b541ec2ecc195497
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-09 13:45:03 +0300

    set up binance lib

---
+++ Skymey-main-lib/Models/Prices/Binance/BinanceCurrentPrice.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Skymey_main_lib.Models.Prices.Binance
+{
+    public class BinanceCurrentPrice
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        [JsonPropertyName("_id")]
+        public ObjectId? _id { get; set; }
+        public string symbol { get; set; }
+        public string price { get; set; }
+        public DateTime Update { get; set; }
+    }
+}
