f0d5b660bb6b346e7db0a1564d9313d319775f9c
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-10 18:04:44 +0300

    create bond lib for tinkoff data

---
+++ Skymey-main-lib/Enums/SecurityTradingStatus.cs
@@ -0,0 +1,90 @@
+﻿using Google.Protobuf.Reflection;
+
+public enum SecurityTradingStatus
+{
+    //
+    // Сводка:
+    //     Торговый статус не определён
+    [OriginalName("SECURITY_TRADING_STATUS_UNSPECIFIED")]
+    Unspecified,
+    //
+    // Сводка:
+    //     Недоступен для торгов
+    [OriginalName("SECURITY_TRADING_STATUS_NOT_AVAILABLE_FOR_TRADING")]
+    NotAvailableForTrading,
+    //
+    // Сводка:
+    //     Период открытия торгов
+    [OriginalName("SECURITY_TRADING_STATUS_OPENING_PERIOD")]
+    OpeningPeriod,
+    //
+    // Сводка:
+    //     Период закрытия торгов
+    [OriginalName("SECURITY_TRADING_STATUS_CLOSING_PERIOD")]
+    ClosingPeriod,
+    //
+    // Сводка:
+    //     Перерыв в торговле
+    [OriginalName("SECURITY_TRADING_STATUS_BREAK_IN_TRADING")]
+    BreakInTrading,
+    //
+    // Сводка:
+    //     Нормальная торговля
+    [OriginalName("SECURITY_TRADING_STATUS_NORMAL_TRADING")]
+    NormalTrading,
+    //
+    // Сводка:
+    //     Аукцион закрытия
+    [OriginalName("SECURITY_TRADING_STATUS_CLOSING_AUCTION")]
+    ClosingAuction,
+    //
+    // Сводка:
+    //     Аукцион крупных пакетов
+    [OriginalName("SECURITY_TRADING_STATUS_DARK_POOL_AUCTION")]
+    DarkPoolAuction,
+    //
+    // Сводка:
+    //     Дискретный аукцион
+    [OriginalName("SECURITY_TRADING_STATUS_DISCRETE_AUCTION")]
+    DiscreteAuction,
+    //
+    // Сводка:
+    //     Аукцион открытия
+    [OriginalName("SECURITY_TRADING_STATUS_OPENING_AUCTION_PERIOD")]
+    OpeningAuctionPeriod,
+    //
+    // Сводка:
+    //     Период торгов по цене аукциона закрытия
+    [OriginalName("SECURITY_TRADING_STATUS_TRADING_AT_CLOSING_AUCTION_PRICE")]
+    TradingAtClosingAuctionPrice,
+    //
+    // Сводка:
+    //     Сессия назначена
+    [OriginalName("SECURITY_TRADING_STATUS_SESSION_ASSIGNED")]
+    SessionAssigned,
+    //
+    // Сводка:
+    //     Сессия закрыта
+    [OriginalName("SECURITY_TRADING_STATUS_SESSION_CLOSE")]
+    SessionClose,
+    //
+    // Сводка:
+    //     Сессия открыта
+    [OriginalName("SECURITY_TRADING_STATUS_SESSION_OPEN")]
+    SessionOpen,
+    //
+    // Сводка:
+    //     Доступна торговля в режиме внутренней ликвидности брокера
+    [OriginalName("SECURITY_TRADING_STATUS_DEALER_NORMAL_TRADING")]
+    DealerNormalTrading,
+    //
+    // Сводка:
+    //     Перерыв торговли в режиме внутренней ликвидности брокера
+    [OriginalName("SECURITY_TRADING_STATUS_DEALER_BREAK_IN_TRADING")]
+    DealerBreakInTrading,
+    //
+    // Сводка:
+    //     Недоступна торговля в режиме внутренней ликвидности брокера
+    [OriginalName("SECURITY_TRADING_STATUS_DEALER_NOT_AVAILABLE_FOR_TRADING")]
+    DealerNotAvailableForTrading
+}
---
+++ Skymey-main-lib/Models/Bonds/Tinkoff/TinkoffBonds.cs
@@ -0,0 +1,104 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.IdGenerators;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Skymey_main_lib.Models.Bonds.Tinkoff
+{
+    public class TinkoffBondAciValue
+    {
+        public string currency { get; set; }
+        public long units { get; set; }
+        public int nano { get; set; }
+    }
+
+    public class TinkoffBondInitialNominal
+    {
+        public string currency { get; set; }
+        public long units { get; set; }
+        public int nano { get; set; }
+    }
+
+    public class TinkoffBondInstrument
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        [JsonPropertyName("_id")]
+        public ObjectId? _id { get; set; }
+        public string figi { get; set; }
+        public string ticker { get; set; }
+        public string classCode { get; set; }
+        public string isin { get; set; }
+        public int? lot { get; set; }
+        public string currency { get; set; }
+        public bool? shortEnabledFlag { get; set; }
+        public string name { get; set; }
+        public string exchange { get; set; }
+        public int? couponQuantityPerYear { get; set; }
+        public Timestamp maturityDate { get; set; }
+        public TinkoffBondNominal nominal { get; set; }
+        public TinkoffBondInitialNominal initialNominal { get; set; }
+        public Timestamp stateRegDate { get; set; }
+        public Timestamp placementDate { get; set; }
+        public TinkoffBondPlacementPrice placementPrice { get; set; }
+        public TinkoffBondAciValue aciValue { get; set; }
+        public string countryOfRisk { get; set; }
+        public string countryOfRiskName { get; set; }
+        public string sector { get; set; }
+        public string issueKind { get; set; }
+        public long? issueSize { get; set; }
+        public long? issueSizePlan { get; set; }
+        public string tradingStatus { get; set; }
+        public bool? otcFlag { get; set; }
+        public bool? buyAvailableFlag { get; set; }
+        public bool? sellAvailableFlag { get; set; }
+        public bool? floatingCouponFlag { get; set; }
+        public bool? perpetualFlag { get; set; }
+        public bool? amortizationFlag { get; set; }
+        public TinkoffBondMinPriceIncrement minPriceIncrement { get; set; }
+        public bool? apiTradeAvailableFlag { get; set; }
+        public string uid { get; set; }
+        public string realExchange { get; set; }
+        public string positionUid { get; set; }
+        public bool? forIisFlag { get; set; }
+        public bool? forQualInvestorFlag { get; set; }
+        public bool? weekendFlag { get; set; }
+        public bool? blockedTcaFlag { get; set; }
+        public bool? subordinatedFlag { get; set; }
+        public bool? liquidityFlag { get; set; }
+        public Timestamp first1minCandleDate { get; set; }
+        public Timestamp first1dayCandleDate { get; set; }
+        public string riskLevel { get; set; }
+        public DateTime Update { get; set; }
+    }
+
+    public class TinkoffBondMinPriceIncrement
+    {
+        public long units { get; set; }
+        public int nano { get; set; }
+    }
+
+    public class TinkoffBondNominal
+    {
+        public string currency { get; set; }
+        public long units { get; set; }
+        public int nano { get; set; }
+    }
+
+    public class TinkoffBondPlacementPrice
+    {
+        public string currency { get; set; }
+        public string units { get; set; }
+        public int nano { get; set; }
+    }
+
+    public class TinkoffBondQuery
+    {
+        public List<TinkoffBondInstrument> instruments { get; set; }
+    }
+}
