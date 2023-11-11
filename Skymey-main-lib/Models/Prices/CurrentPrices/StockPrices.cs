c8eba3a4d91ff94c5dd4f319b823d36f8d731972
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-11 11:59:53 +0300

    add current prices for stock table

---
+++ Skymey-main-lib/Models/Prices/CurrentPrices/StockPrices.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Skymey_main_lib.Models.Prices.StockPrices
+{
+    public class StockPrices
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        [JsonPropertyName("_id")]
+        public ObjectId? _id { get; set; }
+        public string Ticker { get; set; }
+        public string Figi { get; set; }
+        public double Price { get; set; }
+        public DateTime Update { get; set; }
+
+    }
+}
