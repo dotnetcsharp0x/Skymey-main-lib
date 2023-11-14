c767728c30caeeb64ef32ebd6b94fb2f2885ca62
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-14 08:15:33 +0300

    add current prices view model

---
+++ Skymey-main-lib/Models/Prices/CurrentPrices/CurrentPricesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Skymey_main_lib.Models.Prices.CurrentPricesViewModel
+{
+    public class CurrentPricesViewModel
+    {
+        public string Ticker { get; set; }
+        public string Figi { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
+        public DateTime Update { get; set; }
+    }
+}
---
+++ Skymey-main-lib/Models/Prices/CurrentPrices/StockPricesMongo.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Skymey_main_lib.Models.Prices.StockPricesMongo
+{
+    public class StockPricesMongo
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        [JsonPropertyName("_id")]
+        public ObjectId? _id { get; set; }
+        [JsonPropertyName("ticker")]
+        public string Ticker { get; set; }
+        [JsonPropertyName("figi")]
+        public string Figi { get; set; }
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+        [JsonPropertyName("currency")]
+        public string Currency {  get; set; }
+        [JsonPropertyName("update")]
+        public DateTime Update { get; set; }
+    }
+}
