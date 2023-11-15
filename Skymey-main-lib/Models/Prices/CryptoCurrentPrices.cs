fac133f4b1fc829174dec2f645a9450ad0cd89f2
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-15 08:07:58 +0300

    add current prices endpoint for crypto

---
+++ Skymey-main-lib/Models/Prices/CryptoCurrentPrices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Skymey_main_lib.Models.CryptoCurrentPricesView
+{
+    public class CryptoCurrentPricesView
+    {
+        [JsonPropertyName("ticker")]
+        public string Ticker { get; set; }
+        [JsonPropertyName("price")]
+        public double Price { get; set; }
+        [JsonPropertyName("update")]
+        public DateTime Update { get; set; }
+    }
+}
